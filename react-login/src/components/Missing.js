e431265713508bcce729e73035c24da74393a588
Taylor Budd <taylorbudd4@gmail.com>
2024-01-31 17:42:05 -0400

    Implementing 404 page

--- react-login/src/components/Missing.js
+++ react-login/src/components/Missing.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Missing = () => {
   return (
-    <div>Missing</div>
+    <>
+      <div>Sorry! Page not found.</div>
+      <Link to="/">Go back to home</Link>
+    </>
   )
 }
 
