f6aa7023e57413de52aa7c6204ad82dfa80f6db8
Taylor Budd <taylorbudd4@gmail.com>
2024-02-08 17:35:21 -0400

    Wrap private routes with PersistLogin component This will make it so that we do not have to login everytime we visit these pages or refresh them

--- react-login/src/App.js
+++ react-login/src/App.js
@@ -1,17 +1,15 @@
-import Register from './components/Register';
-import Login from './components/Login';
-import Home from './components/Home';
-import Profile from './components/Profile';
-import Missing from './components/Missing';
-import Layout from './components/Layout';
-import Logout from './components/Logout';
-import RequireAuth from './components/RequireAuth';
-import { Routes, Route } from 'react-router-dom';
-import Cookies from 'js-cookie';
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import Missing from "./components/Missing";
+import Layout from "./components/Layout";
+import Logout from "./components/Logout";
+import RequireAuth from "./components/RequireAuth";
+import PersistLogin from "./components/PersistLogin";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
-  const cookie = Cookies.get('user');
 
   return (
     <Routes>
@@ -19,12 +17,14 @@ function App() {
         {/* public routes */}
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        
+
         {/* private routes */}
-        <Route element={<RequireAuth />}>
-          <Route path="/" element={<Home />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/logout' element={<Logout />} />
+        <Route element={<PersistLogin />}>
+          <Route element={<RequireAuth />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/logout" element={<Logout />} />
+          </Route>
         </Route>
 
         <Route path="*" element={<Missing />} />
