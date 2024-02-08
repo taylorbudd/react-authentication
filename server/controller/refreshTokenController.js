02d2c69389c29c5ab7cb723bd0df0b3378a26561
Taylor Budd <taylorbudd4@gmail.com>
2024-02-08 14:24:12 -0400

    Make user async call Send message with any errors

--- server/controller/refreshTokenController.js
+++ server/controller/refreshTokenController.js
@@ -3,22 +3,23 @@ const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 
-function handleRefreshToken(req, res){
-    const cookies = req.cookies
-    if(!cookies?.token) return res.status(401);
+async function handleRefreshToken(req, res){
+    const cookies = req.cookies;
+    if(!cookies?.token){
+        return res.status(401).send("No token");
+    }
 
-    console.log("REFRESH TOKEN: " + cookies.token);
     const refreshToken = cookies.token;
 
-    const user = User.find({refreshToken: refreshToken});
+    const user = await User.findOne({refreshToken: refreshToken});
 
-    if(!user) return res.status(403);
+    if(!user) return res.status(403).send("No user");
 
     jwt.verify(
         refreshToken,
         process.env.REFRESH_TOKEN_SECRET,
         (err, decoded) => {
-            if(err || user.username !== decoded.username) return res.status(403);
+            if(err || user.username !== decoded.username) return res.status(403).send("Invalid token");
             const accessToken = jwt.sign(
                 {"username": decoded.username},
                 process.env.ACCESS_TOKEN_SECRET,
