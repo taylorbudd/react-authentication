fd8e197bdc995bcfaf12b5d7c67760bed9661b09
Taylor Budd <taylorbudd4@gmail.com>
2024-02-08 14:47:46 -0400

    Change middleware to better handle cors use the cors options use cookie parser add verifyJWT middleware to the home route

--- server/server.js
+++ server/server.js
@@ -4,37 +4,45 @@ const mongoose = require('mongoose');
 const verifyJWT = require('./middleware/verifyJWT');
 require('dotenv').config();
 const connectDB = require('./db/db');
+const errorHandler = require('./middleware/errorHandler');
+const corsOptions = require('./config/corsOptions');
+const cookieParser = require('cookie-parser');
+
 
 //Connecting to MongoDB
 connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 //routes
-app.use('/refresh', require('./routes/refresh'));
 app.use('/register', require('./routes/register'));
 app.use('/login', require('./routes/auth'));
 app.use('/logout', require('./routes/logout'));
+app.use('/refresh', require('./routes/refresh'));
 
-//app.use(verifyJWT);
+//private routes
+app.use('/', verifyJWT, require('./routes/home'));
 
-
-app.use((req,res,next) => {
-    console.log("REQUEST: " + JSON.stringify(req.body));
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, Authorization');
-
-    if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true'); // Allow credentials (cookies) to be sent in the request
     next();
 });
 
+app.options('*', (req, res) => {
+    res.status(200)
+        .header('Access-Control-Allow-Credentials', 'true') // Make sure to include this header in the response
+        .end();
+});
+
+app.use(errorHandler);
 
 mongoose.connection.once('open', () => {
     console.log('MongoDB connection established successfully');
